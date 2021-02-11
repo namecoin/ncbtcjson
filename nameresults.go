f9f5aaa8f21ea30ee938e2ed702767789cb96c3c
Jeremy Rand <jeremyrand@airmail.cc>
2021-02-11 10:09:35 +0000

    gofmt

--- nameresults.go
+++ nameresults.go
@@ -7,19 +7,19 @@ package ncbtcjson
 
 // NameShowResult models the data from the name_show command.
 type NameShowResult struct {
-	Name            string   `json:"name"`
-	NameEncoding    Encoding `json:"name_encoding"`
-	NameError       string   `json:"name_error"`
-	Value           string   `json:"value"`
-	ValueEncoding   Encoding `json:"value_encoding"`
-	ValueError      string   `json:"value_error"`
-	TxID            string   `json:"txid"`
-	Vout            uint32   `json:"vout"`
-	Address         string   `json:"address"`
-	IsMine          bool     `json:"ismine"`
-	Height          int32    `json:"height"`
-	ExpiresIn       int32    `json:"expires_in"`
-	Expired         bool     `json:"expired"`
+	Name          string   `json:"name"`
+	NameEncoding  Encoding `json:"name_encoding"`
+	NameError     string   `json:"name_error"`
+	Value         string   `json:"value"`
+	ValueEncoding Encoding `json:"value_encoding"`
+	ValueError    string   `json:"value_error"`
+	TxID          string   `json:"txid"`
+	Vout          uint32   `json:"vout"`
+	Address       string   `json:"address"`
+	IsMine        bool     `json:"ismine"`
+	Height        int32    `json:"height"`
+	ExpiresIn     int32    `json:"expires_in"`
+	Expired       bool     `json:"expired"`
 }
 
 // NameScanResult models the data from the name_scan command.
